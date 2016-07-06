070685ae22528983ba2c6a02c3e8592f74781020
fault <critical@fault.io>
2016-07-05 19:39:54 -0700

    Correct import.

--- extensions/instr/__init__.py
+++ extensions/instr/__init__.py
@@ -5,5 +5,5 @@ LLVM utilities for extracting profile and coverage data.
 	Currently does not provide information about expansion regions.
 """
 from ...probes import libllvm
-from ...probes import libpython
+from ....development.probes import libpython
 __factor_type__ = 'system.extension'
