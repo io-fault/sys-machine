1494686233ca89a39126a7bf64d38e5ebc914a15
fault <critical@fault.io>
2016-07-10 16:59:45 -0700

    Main is importable.
    The exec() was causing the process to terminate.

--- bin/inspect/__main__.py
+++ bin/inspect/__main__.py
@@ -2,5 +2,6 @@ import sys
 import os
 import os.path
 
-exedir = os.path.dirname(__file__)
-os.execl(os.path.join(exedir, '__pycache__', 'host', 'optimal', 'factor'), *sys.argv)
+if __name__ == '__main__':
+	exedir = os.path.dirname(__file__)
+	os.execl(os.path.join(exedir, '__pycache__', 'host', 'optimal', 'factor'), *sys.argv)
