7e09a50d41ff07a907d19a62bd3f7baac3684433
fault <critical@fault.io>
2016-08-08 16:15:27 -0700

    Documentation.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
+"""
+[Executables]
+
+/&.bin.inspect
+	Inspect a source file and extract its immediate structure to standard out as XML.
+
+[Profile Instrumentation]
+
+The coverage data produced by an instrumented binary has a specific format that the LLVM
+libraries can read; the &.instr extension module provides access to data in a merged
+profile file.
+
+For &..development, the (fault.development:role)`measure` role automatically adds the
+necessary flags for enabling the collection of profile data.
+"""
 __factor_type__ = 'project'
