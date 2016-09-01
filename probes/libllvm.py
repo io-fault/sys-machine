925985e7d7a3487238e1d94469d3e96755044e1b
fault <critical@fault.io>
2016-09-01 16:56:42 -0700

    Use subprocess.

--- probes/libllvm.py
+++ probes/libllvm.py
@@ -5,38 +5,48 @@ Gathers information using (system:executable)`llvm-config`.
 """
 import sys
 import sysconfig
-import shell_command
+import subprocess
 
 __factor_type__ = 'system.probe'
+
 parameters = {
 	'executable':
 		"The `llvm-config` executable to extract information from.",
 }
 
-libs_pipe = r"""{} profiledata --libs"""
-syslibs_pipe = r"""{} profiledata --system-libs"""
-incs_pipe = r"""{} --includedir"""
-libdir_pipe = r"""{} --libdir"""
+libs_pipe = ['llvm-config', 'profiledata', '--libs'] #r"""{} profiledata --libs"""
+syslibs_pipe = ['llvm-config', 'profiledata', '--system-libs'] #r"""{} profiledata --system-libs"""
+incs_pipe = ['llvm-config', '--includedir'] #r"""{} --includedir"""
+libdir_pipe = ['llvm-config', '--libdir'] #r"""{} --libdir"""
+rtti_pipe = ['llvm-config', '--has-rtti']
 
 def deploy(*args):
-	# No deployment necessary.
-	libs = shell_command.shell_output(libs_pipe, 'llvm-config')
-	syslibs = shell_command.shell_output(syslibs_pipe, 'llvm-config')
+	Popen = subprocess.Popen
+	STDOUT = subprocess.STDOUT
+	PIPE = subprocess.PIPE
+
+
+	outs = [
+		Popen(libs_pipe, stdin=None, stderr=STDOUT, stdout=PIPE).communicate(),
+		Popen(syslibs_pipe, stdin=None, stderr=STDOUT, stdout=PIPE).communicate(),
+		Popen(libdir_pipe, stdin=None, stderr=STDOUT, stdout=PIPE).communicate(),
+		Popen(incs_pipe, stdin=None, stderr=STDOUT, stdout=PIPE).communicate(),
+		Popen(rtti_pipe, stdin=None, stderr=STDOUT, stdout=PIPE).communicate(),
+	]
+
+	libs, syslibs, libdirs, incdirs, rtti = [x[0].decode('utf-8') for x in outs]
 
 	libs = set(map(str.strip, libs.split('-l')))
 	libs.update(map(str.strip, syslibs.split('-l')))
 	libs.discard('')
 	libs.add('c++')
 
-	libdirs = shell_command.shell_output(libdir_pipe, 'llvm-config')
 	libdirs = set(map(str.strip, libdirs.split('-L')))
 	libdirs.discard('')
 
-	incdirs = shell_command.shell_output(incs_pipe, 'llvm-config')
 	incdirs = set(map(str.strip, incdirs.split('-I')))
 	incdirs.discard('')
 
-	rtti = shell_command.shell_output("{} --has-rtti", 'llvm-config')
 	if rtti.lower() in {'yes', 'true', 'on'}:
 		rtti = True
 	else:
