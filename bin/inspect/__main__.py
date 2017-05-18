f807127bc009552e88253a385220127a483c0f75
fault <critical@fault.io>
2017-05-17 18:34:36 -0700

    Bitrot.

--- bin/inspect/__main__.py
+++ bin/inspect/__main__.py
@@ -4,4 +4,4 @@ import os.path
 
 if __name__ == '__main__':
 	exedir = os.path.dirname(__file__)
-	os.execl(os.path.join(exedir, '__pycache__', 'host', 'optimal', 'factor'), *sys.argv)
+	os.execl(os.path.join(exedir, '__pycache__', 'factor', 'pf.lnk'), *sys.argv)
