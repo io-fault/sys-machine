e4d4d7f166a8c0212250fdd1885bde3f576460d6
fault <critical@fault.io>
2017-05-27 12:31:45 -0700

    Add module execution.

---
+++ xslt/__main__.py
@@ -0,0 +1,15 @@
+"""
+# Executable access to llvm.xslt.
+"""
+import lxml.etree
+import sys
+from ...xml import libfactor
+from ...routes import library as libroutes
+
+if __name__ == '__main__':
+	cmd, target_xml_path = sys.argv
+	xd, xslt = libfactor.xslt(libroutes.Import.from_fullname(__package__).module())
+	del xd
+	i, out = libfactor.transform(xslt, target_xml_path)
+	del i
+	out.write(sys.stdout.buffer)
