4a12188fbf9c03bc85d6697cda2ec01fe3d8a658
fault <critical@fault.io>
2017-07-03 14:44:39 -0700

    Note project parameters for builds.

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 identity = 'http://fault.io/python/llvm'
 name = 'llvm'
-abstract = 'Delineation and coverage instrumentation tools for LLVM builds'
+abstract = 'Delineation and coverage tools for system factors'
 icon = '<http://llvm.org>'
 
 fork = 'green'
@@ -9,3 +9,15 @@ status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
+
+parameters = {
+	# For bin.delineate.
+	'clang-library-path':
+		"Path to the libclang to use.",
+	'clang-includes-path':
+		"Path to include directory containing libclang headers.",
+
+	# For llvm.instr extension module.
+	'llvm-config':
+		"The `llvm-config` executable to extract location information from.",
+}
