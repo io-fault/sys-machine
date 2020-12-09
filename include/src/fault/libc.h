bf522459e51734ba0fbc85d844d78120b8fd7c9e
fault <critical@fault.io>
2020-12-08 21:00:49 -0700

    Rename intentions and trigger injections for coverage and debug.

--- include/src/fault/libc.h
+++ include/src/fault/libc.h
@@ -1,5 +1,5 @@
 /**
-	# fault C construction context environment support.
+	// fault C construction context environment support.
 */
 #ifndef _FAULT_LIBC_H_
 #define _FAULT_LIBC_H_
@@ -27,30 +27,32 @@
 #define F_INTENTION_optimal 1
 #define F_INTENTION_debug 2
 
-#define F_INTENTION_injections 5
-#define F_INTENTION_instruments 10
-
+#define F_INTENTION_coverage 8
 #define F_INTENTION_profiling 4
 
-#define F_INTENTION_fragments (-1)
+/* Negatives used to identify non-functional/informative intents. */
+#define F_INTENTION_delineation (-1)
+#define F_INTENTION_analysis (-2)
 
 #define _F_INTENTION_PREFIX() F_INTENTION_
 #define _F_INTENTION_REF() F_INTENTION
 #undef F_INTENTION_ID
+
+/* 'F_INTENTION_' + F_INTENTION */
 #define F_INTENTION_ID CONCAT_REFERENCES(_F_INTENTION_PREFIX(),_F_INTENTION_REF())
 
 #define F_TRACE(y) 0
 
+/* Switching macros used by applications. */
 #define FV_OPTIMAL(y) (F_INTENTION_ID == F_INTENTION_optimal)
 #define FV_DEBUG(y) (F_INTENTION_ID == F_INTENTION_debug)
 
-#define FV_INJECTIONS(y) \
-	(F_INTENTION_ID == F_INTENTION_injections || F_INTENTION_ID == F_INTENTION_instruments)
-#define FV_INSTRUMENTS(y) (F_INTENTION_ID == F_INTENTION_instruments)
-
+/* Only coverage and debug */
 #define FV_COVERAGE(y) (F_INTENTION_ID == F_INTENTION_coverage)
 #define FV_PROFILING(y) (F_INTENTION_ID == F_INTENTION_profiling)
 
-#define FV_FRAGMENTS(y) (F_INTENTION_ID == F_INTENTION_fragments)
+#define FV_DELINEATION(y) (F_INTENTION_ID == F_INTENTION_delineation)
+#define FV_ANALYSIS(y) (F_INTENTION_ID == F_INTENTION_analysis)
 
+#define FV_INJECTIONS(y) (FV_COVERAGE(y) || FV_DEBUG(y))
 #endif /* guard */
