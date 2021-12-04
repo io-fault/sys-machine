a75c54e320616f63c383f540bef41a4b50ac89e3
fault <critical@fault.io>
2021-12-03 22:35:36 -0700

    Remove SIGNEVER and rename FAULT_SIGNAL_LIST to PROCESS_SIGNAL_LIST.

--- include/src/fault/posix/signal.h
+++ include/src/fault/posix/signal.h
@@ -7,47 +7,42 @@
 #define _FAULT_POSIX_SIGNAL_H_included_
 
 /**
-	// Contrived signal for maintaining invariants
-*/
-#define SIGNEVER -1
-
-/**
 	// Macro providing a list of common signals with
-	// a categorized abstract title.
+	// a categorized descriptive name.
 */
-#define FAULT_SIGNAL_LIST() \
+#define PROCESS_SIGNAL_LIST() \
+	SIG(INT, process, interrupt) \
 	SIG(CHLD, process, status) \
 	SIG(STOP, process, stop) \
 	SIG(CONT, process, continue) \
 	SIG(TERM, process, terminate) \
-	SIG(INT, process, interrupt) \
 	SIG(KILL, process, kill) \
 	SIG(QUIT, process, quit) \
 	\
 	SIG(XCPU, limit, cpu) \
 	SIG(XFSZ, limit, file) \
 	SIG(ALRM, limit, time) \
-	SIG(VTALRM, limit, virtual) \
 	SIG(PROF, limit, profiling) \
+	SIG(VTALRM, limit, virtual) \
 	\
 	SIG(HUP, terminal, closed) \
 	SIG(TSTP, terminal, stop) \
 	SIG(INFO, terminal, query) \
-	SIG(WINCH, terminal, delta) \
 	SIG(TTIN, terminal, background-read) \
 	SIG(TTOU, terminal, background-write) \
+	SIG(WINCH, terminal, delta) \
 	\
+	SIG(IO, event, io) \
+	SIG(URG, event, urgent-condition) \
+	SIG(TRAP, event, trap) \
 	SIG(USR1, event, user-1) \
 	SIG(USR2, event, user-2) \
-	SIG(URG, event, urgent-condition) \
-	SIG(IO, event, io) \
-	SIG(TRAP, event, trap) \
 	\
 	SIG(FPE, exception, floating-point) \
 	SIG(PIPE, exception, broken-pipe) \
 	\
-	SIG(BUS, error, invalid-memory-access) \
 	SIG(SEGV, error, restricted-memory-access) \
+	SIG(BUS, error, invalid-memory-access) \
 	SIG(ILL, error, invalid-instruction) \
 	SIG(SYS, error, invalid-system-call)
 #endif
